dd3fedf7f88428c1e43915205cc47907f3cfb311
daan-vlh <s148821@ap.be>
2025-10-14 11:24:35 +0200

    implement backend

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,78 +1,92 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Image } from "expo-image";
+import { StyleSheet } from "react-native";
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import ParallaxScrollView from "@/components/parallax-scroll-view";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import {
+  InstrumentSerif_400Regular,
+  useFonts,
+} from "@expo-google-fonts/instrument-serif";
+import { Link } from "expo-router";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function HomeScreen() {
+  const [fontsLoaded] = useFonts({
+    InstrumentSerif_400Regular,
+  });
+
+  // Basic SWR usage
+  const { data, error, isLoading } = useSWR(
+    "https://data-app-bs97.onrender.com/messages",
+    fetcher
+  );
+
+  if (!fontsLoaded) {
+    return null; // or a loading indicator
+  }
+
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require("@/assets/images/partial-react-logo.png")}
           style={styles.reactLogo}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
+        <ThemedText style={{ color: "#5fb6fdff" }} type="subtitle">
+          Mango
         </ThemedText>
       </ThemedView>
+      <ThemedView>
+        <Image
+          source={require("@/assets/images/successful-entrepreneurs.jpg")}
+          style={styles.heroImage}
+        ></Image>
+      </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
+        <ThemedText style={{ marginBottom: 8 }} type="title">
+          SWR Data Fetching
+        </ThemedText>
+        <Link
+          href={"../settings"}
+          style={{
+            color: "white",
+            textDecorationLine: "underline",
+          }}
+        >
+          Go to login page
         </Link>
-
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+        {isLoading && <ThemedText>Loading...</ThemedText>}
+        {error && <ThemedText>Error loading data</ThemedText>}
+        {Array.isArray(data) &&
+          data.length > 0 &&
+          data.map((message, idx) => {
+            return (
+              <ThemedView
+                key={message._id ?? idx}
+                style={{
+                  marginBottom: 10,
+                  backgroundColor: "#ffffff1a",
+                  padding: 10,
+                  borderRadius: 5,
+                }}
+              >
+                <ThemedText>{message.text}</ThemedText>
+                <ThemedText style={{ fontSize: 14, fontStyle: "italic" }}>
+                  Sender: {message.sender.username}
+                </ThemedText>
+                <ThemedText style={{ fontSize: 14, color: "#888" }}>
+                  Date: {new Date(message.createdAt).toLocaleString()}{" "}
+                </ThemedText>
+              </ThemedView>
+            );
+          })}
       </ThemedView>
     </ParallaxScrollView>
   );
@@ -80,8 +94,8 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -93,6 +107,12 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
+  },
+  heroImage: {
+    width: "100%",
+    aspectRatio: 16 / 9,
+    borderRadius: 8,
+    resizeMode: "cover",
   },
 });
